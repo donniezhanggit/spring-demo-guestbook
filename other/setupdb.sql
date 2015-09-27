db86b600e94f786f16056d6df1c41ded14d36cfd
Whitesquall <whitesquall@gmx.com>
2015-09-27 19:05:18 +0300

    Small fix for setupdb.sql.

--- other/setupdb.sql
+++ other/setupdb.sql
@@ -14,7 +14,7 @@ CREATE TABLE comments (
 
 INSERT INTO comments (created, name, message)
        VALUES ('2015-09-19 14:00:00', 'admin', 'Welcome to this demo application.'),
-              ('2015-09-19 14:00:01', 'admin', 'Runned up using Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2 and Zenburn.'),
+              ('2015-09-19 14:00:01', 'admin', 'Used technologies: Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2, Maven 3 and Zenburn.'),
               ('2015-09-19 14:00:02', 'admin', 'You can grab source code freely from github under GPL terms.');
 
 COMMIT TRANSACTION;
