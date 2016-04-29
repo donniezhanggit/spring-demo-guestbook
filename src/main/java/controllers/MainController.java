c8fe5f81e0ee53d0c0e87ce01f753bae53f8116a
Whitesquall <whitesquall@gmx.com>
2016-04-29 23:25:10 +0300

    Cleaning.

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,7 +34,7 @@ public class MainController extends WebMvcConfigurerAdapter {
 	}
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView listComments(Model model) {
+	public ModelAndView listComments() {
 		return this.generateView("list", new CommentForm());
 	}
 
