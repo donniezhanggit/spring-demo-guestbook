cb960be8edf619cee9dbf362467d0ed9cd123c09
Whitesquall <whitesquall@gmx.com>
2016-04-29 23:16:46 +0300

    Fixed build.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,6 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages="demo.models")
 public class Application {
     public static void main(String[] args) throws Exception {
-    	SpringApplication.run(DemoApplication.class, args);
+    	SpringApplication.run(Application.class, args);
     }
 }
