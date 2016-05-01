d17f304344c9658c4a2f0764eba8b454d5134ac6
Whitesquall <whitesquall@gmx.com>
2016-05-01 14:20:23 +0300

    Project cleaning.

--- src/main/java/demo/repos/CommentRepository.java
+++ src/main/java/demo/repos/CommentRepository.java
@@ -3,6 +3,7 @@ package demo.repos;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import demo.models.Comment;
 
