3f8205666d4cf3b6dcc966ecdb4365234af39bac
Whitesquall <whitesquall@gmx.com>
2016-05-01 20:49:55 +0300

    Fixed bootstrap sql data script.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,15 +1,8 @@
 -- Database setup script for h2.
 
-DROP TABLE IF EXISTS comments;
-
-CREATE TABLE comments (
-       id SERIAL PRIMARY KEY,
-       created TIMESTAMP NOT NULL,
-       name VARCHAR(20) NOT NULL,
-       message VARCHAR(2048) NOT NULL
-);
-
-INSERT INTO comments (created, name, message)
-       VALUES ('2015-09-19 14:00:00', 'admin', 'Welcome to this demo application.'),
-              ('2015-09-19 14:00:01', 'admin', 'Used technologies: Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2, Maven 3 and Zenburn.'),
-              ('2015-09-19 14:00:02', 'admin', 'You can grab source code freely from github under GPL terms.');
+INSERT INTO comments
+    (created, name, message)
+VALUES
+    ('2015-09-19 14:00:00', 'admin', 'Welcome to this demo application.'),
+    ('2015-09-19 14:00:01', 'admin', 'Used technologies: Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2, Maven 3 and Zenburn.'),
+    ('2015-09-19 14:00:02', 'admin', 'You can grab source code freely from github under GPL terms.');
