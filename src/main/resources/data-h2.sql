686231e3a35e8321ade609b05cb500b3e0775118
Whitesquall <whitesquall@gmx.com>
2016-05-14 13:24:04 +0300

    Added test data with user.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,8 +1,13 @@
 -- Database setup script for h2.
 
-INSERT INTO comment
-    (created, name, message)
+INSERT INTO user
+    (id, username, password, email, created, active)
 VALUES
-    ('2015-09-19 14:00:00', 'admin', 'Welcome to this demo application.'),
-    ('2015-09-19 14:00:01', 'admin', 'Used technologies: Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2, Maven 3 and Zenburn.'),
-    ('2015-09-19 14:00:02', 'admin', 'You can grab source code freely from github under GPL terms.');
+    (1, 'user', 'password', 'user@spring-gb13.com', '2016-01-01', 1);
+
+INSERT INTO comment
+    (created, name, message, user_id)
+VALUES
+    ('2015-09-19 14:00:00', 'admin', 'Welcome to this demo application.', 1),
+    ('2015-09-19 14:00:01', 'admin', 'Used technologies: Java 7, Spring 4 (Boot, MVC), Hibernate 4, Thymeleaf 2, Maven 3 and Zenburn.', 1),
+    ('2015-09-19 14:00:02', 'admin', 'You can grab source code freely from github under GPL terms.', 1);
