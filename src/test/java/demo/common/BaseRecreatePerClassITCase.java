9b7857a2e83601f8944803b4b179651e5cf60821
Whitesquall <whitesquall@gmx.com>
2017-07-29 18:57:55 +0300

    Added commit annotation.

--- src/test/java/demo/common/BaseRecreatePerClassITCase.java
+++ src/test/java/demo/common/BaseRecreatePerClassITCase.java
@@ -1,9 +1,11 @@
 package demo.common;
 
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 
+@Commit
 @DirtiesContext(classMode=ClassMode.AFTER_CLASS)
 public abstract class BaseRecreatePerClassITCase extends AbstractITCase {
 
