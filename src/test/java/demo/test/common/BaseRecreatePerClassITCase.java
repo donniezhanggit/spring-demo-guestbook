617b9e1bc07023c394c0c253a234aadc1733d69a
Whitesquall <whitesquall@gmx.com>
2017-07-30 16:53:36 +0300

    Small fix.

--- src/test/java/demo/test/common/BaseRecreatePerClassITCase.java
+++ src/test/java/demo/test/common/BaseRecreatePerClassITCase.java
@@ -1,11 +1,9 @@
 package demo.test.common;
 
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 
-@Commit
 @DirtiesContext(classMode=ClassMode.AFTER_CLASS)
 public abstract class BaseRecreatePerClassITCase extends AbstractITCase {
 
