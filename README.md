a862de665bbda444bc39fa054ae240950d83b946
Whitesquall <whitesquall@gmx.com>
2017-08-13 13:30:49 +0300

    Removed supporting of openshift.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # spring-demo-guestbook
 Demo project using Java 8, Spring 4 (Boot, MVC, JPA, Test), Hibernate 5, Thymeleaf 2, Maven 3.
-
-Do you want to touch this demo? [Go here!](https://spring-gb13.rhcloud.com/)
--- src/main/java/demo/config/GuestBookProfiles.java
+++ src/main/java/demo/config/GuestBookProfiles.java
@@ -2,7 +2,6 @@ package demo.config;
 
 
 public class GuestBookProfiles {
-    public static final String OPENSHIFT = "openshift";
     public static final String DEVELOPMENT = "dev";
     public static final String PG_INTEGRATION_TESTING = "pgit";
     public static final String H2_INTEGRATION_TESTING = "h2it";
