6ecc23cd93ac27df2c84e1b9025a8c1d76f5034c
Whitesquall <whitesquall@gmx.com>
2017-08-13 12:31:34 +0300

    Clean up imports.

--- src/main/java/demo/model/Comment.java
+++ src/main/java/demo/model/Comment.java
@@ -3,12 +3,10 @@ package demo.model;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.Immutable;
