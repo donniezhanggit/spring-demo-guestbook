824f3ae9e90e570b7081e69424490f9364583135
Whitesquall <whitesquall@gmx.com>
2017-08-18 21:33:31 +0300

    Fixed imports.

--- src/test/java/demo/test/it/common/BaseEndpointCase.java
+++ src/test/java/demo/test/it/common/BaseEndpointCase.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
