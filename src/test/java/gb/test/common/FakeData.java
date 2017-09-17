293e02c42b54ecbb8852dfc86feb977fb1202437
Whitesquall <whitesquall@gmx.com>
2017-09-17 12:31:57 +0300

    Added method for email generation.

--- src/test/java/gb/test/common/FakeData.java
+++ src/test/java/gb/test/common/FakeData.java
@@ -6,4 +6,13 @@ public class FakeData {
     public static String stringWithLength(int length) {
         return StringUtils.repeat("A", length);
     }
+
+    public static String emailWithLength(int length) {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(stringWithLength(length));
+        sb.setCharAt(length / 2, '@');
+
+        return sb.toString();
+    }
 }
