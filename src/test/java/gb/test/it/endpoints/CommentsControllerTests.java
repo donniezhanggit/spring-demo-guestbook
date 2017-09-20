9b039365464887f01ae4d09c6ddf113af0c755c1
Whitesquall <whitesquall@gmx.com>
2017-09-20 20:56:50 +0300

    Added new tests for call verification.

--- src/test/java/gb/test/it/endpoints/CommentsControllerTests.java
+++ src/test/java/gb/test/it/endpoints/CommentsControllerTests.java
@@ -9,9 +9,8 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
 import gb.api.CommentsApi;
@@ -23,20 +22,22 @@ import gb.test.it.common.EndpointITCase;
 
 
 public class CommentsControllerTests extends EndpointITCase {
-    private static final Long EXISTING_ID = 1L;
-    private static final Long NON_EXISTENT_ID = -1L;
-    private static final Short VERSION = 0;
+    private static final long EXISTING_ID = 1L;
+    private static final long NON_EXISTENT_ID = Long.MAX_VALUE;
+    private static final short VERSION = 0;
     private static final String NAME = "anon";
     private static final String MESSAGE = "message";
     private static final LocalDateTime CREATED = LocalDateTime.now();
     private static final String COMMENTS_API_URL = "/api/comments/";
 
-    @Autowired
+    @MockBean
     private CommentsApi commentsApi;
 
 
     @Override
     public void setup() {
+        MockitoAnnotations.initMocks(this);
+
         final CommentEntry commentEntry = this.buildAnonCommentEntry();
 
         when(this.commentsApi.createComment(any(CommentInput.class)))
@@ -57,6 +58,16 @@ public class CommentsControllerTests extends EndpointITCase {
             .andExpect(status().isOk())
             .andExpect(content()
                     .contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+
+    @Test
+    public void Getting_a_list_of_comments_should_call_APIs_getComments()
+            throws Exception {
+        this.mockMvc.perform(get(COMMENTS_API_URL));
+
+        verify(this.commentsApi, times(1)).getComments();
+        verifyNoMoreInteractions(this.commentsApi);
     }
 
 
@@ -83,6 +94,22 @@ public class CommentsControllerTests extends EndpointITCase {
 
 
     @Test
+    public void Getting_a_non_existent_comment_should_call_APIs_getComment()
+            throws Exception {
+        final String url = COMMENTS_API_URL + NON_EXISTENT_ID;
+
+        this.mockMvc.perform(get(url));
+
+        System.out.println("CLASS CLASS CLASS");
+        System.out.println(this.commentsApi.getClass());
+        System.out.println("CLASS CLASS CLASS");
+
+        verify(this.commentsApi, times(1)).getComment(NON_EXISTENT_ID);
+        verifyNoMoreInteractions(this.commentsApi);
+    }
+
+
+    @Test
     public void Creating_a_new_comment_should_return_201() throws Exception {
         final String jsonComment = this.jsonify(this.buildAnonCommentInput());
 
@@ -92,6 +119,22 @@ public class CommentsControllerTests extends EndpointITCase {
             .andExpect(status().isCreated())
             .andExpect(content()
                     .contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+
+    @Test
+    public void Creating_a_new_comment_should_call_APIs_createComment()
+            throws Exception {
+        final CommentInput input = this.buildAnonCommentInput();
+        final String jsonComment = this.jsonify(input);
+
+        this.mockMvc.perform(post(COMMENTS_API_URL)
+                .content(jsonComment)
+                .contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        verify(this.commentsApi, times(1))
+            .createComment(any(CommentInput.class));
+        verifyNoMoreInteractions(this.commentsApi);
     }
 
 
@@ -106,14 +149,5 @@ public class CommentsControllerTests extends EndpointITCase {
     private CommentInput buildAnonCommentInput() {
         return new CommentInputBuilder()
                 .name(NAME).message(MESSAGE).build();
-    }
-
-
-    @TestConfiguration
-    public static class Config {
-        @Bean
-        public CommentsApi commentsApi() {
-            return mock(CommentsApi.class);
-        }
     }
 }
