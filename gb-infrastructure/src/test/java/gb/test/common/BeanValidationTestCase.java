26368c906cc21cd2ec1bf0e40a269b947381c76b
Whitesquall <whitesquall@gmx.com>
2018-04-11 00:31:55 +0300

    Added basic bean validation test case.

---
+++ gb-infrastructure/src/test/java/gb/test/common/BeanValidationTestCase.java
@@ -0,0 +1,19 @@
+package gb.test.common;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+
+public class BeanValidationTestCase extends JUnitTestCase {
+        protected <T> Set<ConstraintViolation<T>> validate(T bean) {
+                final ValidatorFactory factory =
+                                Validation.buildDefaultValidatorFactory();
+                final Validator validator = factory.getValidator();
+
+                return validator.validate(bean);
+        }
+}
