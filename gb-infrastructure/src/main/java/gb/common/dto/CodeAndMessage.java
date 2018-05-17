e8f36284f918962443d02c90658460541ce195b5
Whitesquall <whitesquall@gmx.com>
2018-05-18 01:09:55 +0300

    Code simplification.

--- gb-infrastructure/src/main/java/gb/common/dto/CodeAndMessage.java
+++ gb-infrastructure/src/main/java/gb/common/dto/CodeAndMessage.java
@@ -1,13 +1,26 @@
 package gb.common.dto;
 
-import lombok.AccessLevel;
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+import javax.annotation.Nonnull;
+import javax.validation.ConstraintViolation;
+
+import lombok.AllArgsConstructor;
 import lombok.Value;
 import lombok.experimental.FieldDefaults;
 
 
 @Value
-@FieldDefaults(level=AccessLevel.PRIVATE, makeFinal=true)
+@FieldDefaults(level=PRIVATE, makeFinal=true)
+@AllArgsConstructor(access=PROTECTED)
 public class CodeAndMessage {
-	String code;
-	String message;
+    String code;
+    String message;
+
+
+    public CodeAndMessage(@Nonnull final ConstraintViolation<?> cv) {
+        code = cv.getPropertyPath().toString();
+        message = cv.getMessage();
+    }
 }
--- gb-infrastructure/src/main/java/gb/common/dto/Errors.java
+++ gb-infrastructure/src/main/java/gb/common/dto/Errors.java
@@ -1,30 +1,28 @@
 package gb.common.dto;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nonnull;
 import javax.validation.ConstraintViolationException;
 
-import lombok.AccessLevel;
 import lombok.Value;
-import lombok.val;
 import lombok.experimental.FieldDefaults;
 
 
 @Value
-@FieldDefaults(level=AccessLevel.PRIVATE, makeFinal=true)
+@FieldDefaults(level=PRIVATE, makeFinal=true)
 public class Errors {
     List<CodeAndMessage> errors;
 
 
-    public Errors(ConstraintViolationException e) {
-        val constraintViolations = e.getConstraintViolations();
-
-        errors = constraintViolations.stream()
-                        .filter(cv -> cv != null)
-                .map(cv -> new CodeAndMessage(
-                                cv.getPropertyPath().toString(),
-                                cv.getMessage()
-                )).collect(Collectors.toList());
+    public Errors(@Nonnull final ConstraintViolationException e) {
+        errors = e.getConstraintViolations().stream()
+            .filter(Objects::nonNull)
+            .map(CodeAndMessage::new)
+            .collect(Collectors.toList());
     }
 }
