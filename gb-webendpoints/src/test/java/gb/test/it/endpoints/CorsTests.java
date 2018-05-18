ee06c3218462d8bd42e8e2365a03eb9affede228
Whitesquall <whitesquall@gmx.com>
2018-05-18 11:27:28 +0300

    Removed ping controller.
    And created test specific controller.

--- gb-webendpoints/src/test/java/gb/test/it/endpoints/CorsTests.java
+++ gb-webendpoints/src/test/java/gb/test/it/endpoints/CorsTests.java
@@ -1,17 +1,21 @@
 package gb.test.it.endpoints;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import gb.controllers.PingController;
 import gb.test.it.common.EndpointITCase;
 
 
-@WebMvcTest(PingController.class)
+@WebMvcTest(CorsTests.TestController.class)
+@WithMockUser(username="testUser", roles={"USER", "ADMIN", "ACTUATOR"})
 public class CorsTests extends EndpointITCase {
     private static final String PING_API_URL = "/api/ping";
     private static final String ORIGIN_HEADER = "Origin";
@@ -20,7 +24,6 @@ public class CorsTests extends EndpointITCase {
 
 
     @Test
-    @WithMockUser(username="testUser", roles={"USER", "ADMIN", "ACTUATOR"})
     public void Request_with_wrong_origin_should_return_403()
             throws Exception {
         mockMvc.perform(get(PING_API_URL)
@@ -30,11 +33,19 @@ public class CorsTests extends EndpointITCase {
 
 
     @Test
-    @WithMockUser(username="testUser", roles={"USER", "ADMIN", "ACTUATOR"})
     public void Request_with_right_origin_should_return_200()
             throws Exception {
         mockMvc.perform(get(PING_API_URL)
                 .header(ORIGIN_HEADER, RIGHT_ORIGIN_URL))
             .andExpect(status().isOk());
+    }
+
+
+    @RestController
+    public static class TestController {
+        @GetMapping(path=PING_API_URL, produces=APPLICATION_JSON_UTF8_VALUE)
+        public ResponseEntity<String> pong() {
+            return ResponseEntity.ok(null);
+        }
     }
 }
