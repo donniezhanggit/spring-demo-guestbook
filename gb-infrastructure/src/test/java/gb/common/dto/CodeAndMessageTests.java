ad0fbed5c024a470f9e569e8551ff5b0e719d877
Whitesquall <whitesquall@gmx.com>
2018-05-30 15:52:45 +0300

    Added tests for CodeAndMessage.

--- gb-infrastructure/src/test/java/gb/common/dto/CodeAndMessageTests.java
+++ gb-infrastructure/src/test/java/gb/common/dto/CodeAndMessageTests.java
@@ -1,5 +1,6 @@
 package gb.common.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 import javax.validation.ConstraintViolation;
@@ -12,6 +13,11 @@ import lombok.val;
 
 
 public class CodeAndMessageTests extends JUnitTestCase {
+    private static final String CODE = "code";
+    private static final String BAD_REQUEST_CODE = "BAD_REQUEST";
+    private static final String MESSAGE = "message";
+
+
     @Test
     public void When_ConstraintViolation_is_null_should_throw_NPE() {
         ConstraintViolation<?> _null = (ConstraintViolation<?>) null;
@@ -36,5 +42,29 @@ public class CodeAndMessageTests extends JUnitTestCase {
 
         assertThatNullPointerException()
             .isThrownBy(() -> new CodeAndMessage(_null));
+    }
+
+
+    @Test
+    public void IllegalArgumentException_should_map_to_CodeAndMessage() {
+        // Act.
+        val com = new CodeAndMessage(new IllegalArgumentException(MESSAGE));
+
+        // Assert.
+        assertThat(com.getCode()).isEqualTo(BAD_REQUEST_CODE);
+        assertThat(com.getMessage()).isEqualTo(MESSAGE);
+    }
+
+
+    @Test
+    public void InvalidArgumentException_shlould_map_to_CodeAndMessage() {
+        // Act.
+        val com = new CodeAndMessage(
+                new InvalidArgumentException(CODE, MESSAGE)
+        );
+
+        // Assert.
+        assertThat(com.getCode()).isEqualTo(CODE);
+        assertThat(com.getMessage()).isEqualTo(MESSAGE);
     }
 }
