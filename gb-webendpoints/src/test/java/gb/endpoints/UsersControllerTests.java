d872be2c1322d7e6e88f141d9fbe2e1eae4927ec
Whitesquall <whitesquall@gmx.com>
2018-06-01 18:35:24 +0300

    Added tests for UsersController.

--- gb-webendpoints/src/test/java/gb/endpoints/UsersControllerTests.java
+++ gb-webendpoints/src/test/java/gb/endpoints/UsersControllerTests.java
@@ -1,5 +1,6 @@
 package gb.endpoints;
 
+import static gb.testlang.fixtures.FullNameFixtures.buildFullNameInput;
 import static gb.testlang.fixtures.UsersFixtures.EXISTING_USERNAME;
 import static gb.testlang.fixtures.UsersFixtures.NON_EXISTENT_USERNAME;
 import static gb.testlang.fixtures.UsersFixtures.buildUserEntry;
@@ -12,14 +13,15 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -29,7 +31,6 @@ import gb.common.it.EndpointITCase;
 import gb.controllers.UsersController;
 import gb.dto.FullNameInput;
 import gb.dto.UserEntry;
-import gb.testlang.fixtures.UsersFixtures;
 import lombok.experimental.FieldDefaults;
 
 
@@ -65,7 +66,7 @@ public class UsersControllerTests extends EndpointITCase {
         // Arrange.
         final String url = USERS_API_URL + EXISTING_USERNAME;
 
-        // Act.
+        // Act and assert.
         mockMvc.perform(get(url))
             .andExpect(status().isOk())
             .andExpect(content().contentType(APPLICATION_JSON_UTF8));
@@ -93,7 +94,7 @@ public class UsersControllerTests extends EndpointITCase {
         // Arrange.
         final String url = USERS_API_URL + NON_EXISTENT_USERNAME;
 
-        // Act.
+        // Act and assert.
         mockMvc.perform(get(url))
             .andExpect(status().isNotFound());
     }
@@ -111,5 +112,88 @@ public class UsersControllerTests extends EndpointITCase {
         // Assert.
         verify(usersApi, times(1)).getUser(NON_EXISTENT_USERNAME);
         verifyNoMoreInteractions(usersApi);
+    }
+
+
+    @Test
+    public void Deactivating_of_user_should_return_204() throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/blocked";
+
+        // Act and assert.
+        mockMvc.perform(put(url))
+            .andExpect(status().isNoContent());
+    }
+
+
+    @Test
+    public void Deactivating_of_user_should_call_APIs_deactivateUser()
+            throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/blocked";
+
+        // Act.
+        mockMvc.perform(put(url));
+
+        // Assert.
+        verify(usersApi, times(1)).deactivateUser(EXISTING_USERNAME);
+    }
+
+
+    @Test
+    public void Activating_of_user_should_return_204() throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/blocked";
+
+        // Act and assert.
+        mockMvc.perform(delete(url))
+            .andExpect(status().isNoContent());
+    }
+
+
+    @Test
+    public void Activating_of_user_should_call_APIs_deactivateUser()
+            throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/blocked";
+
+        // Act.
+        mockMvc.perform(delete(url));
+
+        // Assert.
+        verify(usersApi, times(1)).activateUser(EXISTING_USERNAME);
+    }
+
+
+    @Test
+    public void Changing_fullName_should_return_204() throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/fullName";
+        final FullNameInput input = buildFullNameInput();
+        final String inputAsJson = jsonify(input);
+
+        // Act and assert.
+        mockMvc.perform(put(url)
+                .content(inputAsJson)
+                .contentType(APPLICATION_JSON_UTF8))
+            .andExpect(status().isNoContent());
+    }
+
+
+    @Test
+    public void Changing_fullName_should_call_APIs_changeName()
+            throws Exception {
+        // Arrange.
+        final String url = USERS_API_URL + EXISTING_USERNAME + "/fullName";
+        final FullNameInput input = buildFullNameInput();
+        final String inputAsJson = jsonify(input);
+
+        // Act.
+        mockMvc.perform(put(url)
+                .content(inputAsJson)
+                .contentType(APPLICATION_JSON_UTF8));
+
+        // Assert.
+        verify(usersApi, times(1)).changeName(EXISTING_USERNAME, input);
     }
 }
