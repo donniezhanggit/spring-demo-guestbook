0bf33d404058e186a33d28ee5d1c425ee5553d6c
Whitesquall <whitesquall@gmx.com>
2018-06-01 15:37:24 +0300

    Dislike to UtilityClass.

--- gb-core/src/test/java/gb/testlang/fixtures/FullNameFixtures.java
+++ gb-core/src/test/java/gb/testlang/fixtures/FullNameFixtures.java
@@ -3,28 +3,26 @@ package gb.testlang.fixtures;
 import gb.dto.FullNameInput;
 import gb.dto.FullNameInputBuilder;
 import gb.model.FullName;
-import lombok.experimental.UtilityClass;
 
 
-@UtilityClass
 public class FullNameFixtures {
     public static final String FIRST_NAME = "John";
     public static final String LAST_NAME = "Dow";
 
 
-    public FullNameInputBuilder filledFullNameBuilder() {
+    public static FullNameInputBuilder filledFullNameBuilder() {
         return new FullNameInputBuilder()
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME);
     }
 
 
-    public FullNameInput buildFullNameInput() {
+    public static FullNameInput buildFullNameInput() {
         return filledFullNameBuilder().build();
     }
 
 
-    public FullName buildFullName() {
+    public static FullName buildFullName() {
         return new FullName(FIRST_NAME, LAST_NAME);
     }
 }
