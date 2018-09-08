ca1e96f7cc07b13f9b04b32c6e7d75b49aec369f
Whitesquall <whitesquall@gmx.com>
2018-09-08 19:04:47 +0300

    Small fix.

--- gb-core/src/main/java/gb/services/NewCommentsLogger.java
+++ gb-core/src/main/java/gb/services/NewCommentsLogger.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly=true)
 @AllArgsConstructor
 @FieldDefaults(level=PRIVATE, makeFinal=true)
-class NewCommentsLogger {
+public class NewCommentsLogger {
     @NonNull CommentsRepository commentsRepo;
 
 
