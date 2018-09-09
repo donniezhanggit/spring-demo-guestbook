3e0460297b7f8cea71cc84ea099e4f28cf3e273e
Whitesquall <whitesquall@gmx.com>
2018-09-09 12:20:56 +0300

    Include subtype in events serialization.

--- gb-infrastructure/src/main/java/gb/common/events/DomainEvent.java
+++ gb-infrastructure/src/main/java/gb/common/events/DomainEvent.java
@@ -3,7 +3,11 @@ package gb.common.events;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
+
+@JsonTypeInfo(use=Id.NAME, property="type")
 public interface DomainEvent {
     UUID getId();
     LocalDateTime getCreatedAt();
